1062c4524c8f2fa166ab6e155f5186cf8a7bd88d
Sean Lynch <lynchie42@gmail.com>
2025-12-03 21:41:09 +0000

    day 1 complete

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -4,32 +4,89 @@ const parseInput = (rawInput: string) => rawInput;
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const lines = input.split("\n");
+  let sum = 50;
+  let zeros = 0;
+  for (const line of lines) {
+    const [direction, amount] = [line.charAt(0), parseInt(line.substring(1))];
+    sum = (sum + amount * (direction == "L" ? -1 : 1)) % 100;
 
-  return;
+    if (sum == 0) {
+      zeros++;
+    }
+  }
+  return zeros.toString();
 };
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const lines = input.split("\n");
+  let sum = 50;
+  let zeros = 0;
+  for (const line of lines) {
+    const [direction, amount] = [line.charAt(0), parseInt(line.substring(1))];
+    const modifier = direction == "L" ? -1 : 1;
+    const fullTurns = Math.floor(amount / 100);
+    zeros += fullTurns;
 
-  return;
+    const extraTurns = amount % 100;
+    const relativeSum = sum + modifier * extraTurns;
+    if (sum != 0 && (relativeSum <= 0 || relativeSum >= 100)) {
+      zeros++;
+    }
+
+    sum = (100 + relativeSum) % 100;
+  }
+  return zeros.toString();
 };
 
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`,
+        expected: "3",
+      },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82`,
+        expected: "6",
+      },
+      {
+        input: `L68
+L30
+R148
+L5
+R60
+L55
+L1
+L99
+R14
+L82`,
+        expected: "7",
+      },
     ],
     solution: part2,
   },
